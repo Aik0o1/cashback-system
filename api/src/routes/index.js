27dd60dd24da08fe24af2e61e93509c1e86172ba
Aik0o1 <anesroticv@gmail.com>
2024-10-10 16:04:14 -0300

    updat

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,7 +6,7 @@ import produtoRoutes from "./produtoRoutes.js"
 import cors from "cors"
 const routes = (app) => {
     app.route("/").get((req,res) => res.status(200).send("Olá mundo"))
-    app.use(cors({ origin: 'http://localhost:5173' }))
+    // app.use(cors({ origin: 'http://localhost:5173' }))
     app.use(express.json(), userRoutes)
     app.use(express.json(), empresarioRoutes)
     app.use(express.json(), produtoRoutes)
